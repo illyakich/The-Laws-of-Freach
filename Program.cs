41beaa18cae5f7b98e5914af3a129a3a8b6e6c6c
illyakich <speedfeed123@gmail.com>
2024-09-27 22:31:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+foreach (int index in array)
+
+if (index < currentSmallest)
+        currentSmallest = array[index];
+
+Console.WriteLine(currentSmallest); 
+
+// for this challenge you only had to modify some code in the book to convert it from a for loop into a foreach loop.
